88b719995dfbd54be4ec0e87edb431cb16e01e41
khushikumari226 <160056189+khushikumari226@users.noreply.github.com>
2024-02-14 23:40:08 +0530

    Add files via upload

---
+++ codsoft/calculator/script.js
@@ -0,0 +1,27 @@
+let string = "";
+let button = document.querySelectorAll('.button');
+Array.from(button).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'M+'){
+            string = "error";
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'M-'){
+            string = "error";
+            document.querySelector('input').value = string;
+        }
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
